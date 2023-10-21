ebe6ca72030a1ec00bffeae7c3e50bbab407dcd3
Zanzao Chen <zanzaoc@andrew.cmu.edu>
2023-10-21 14:15:10 -0400

    deleted files for react migration

--- feedback.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const chosenLocation = localStorage.getItem("chosen location")
-    let chosenLocationDisplay = document.getElementById('chosenLocationDisplay')
-    chosenLocationDisplay.textContent = chosenLocation
-});
