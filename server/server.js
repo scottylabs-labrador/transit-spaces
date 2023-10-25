5b881dcf1bf5fbbdd1faff8bdd06a6ff03f40ba5
Zanzao Chen <zanzaoc@andrew.cmu.edu>
2023-10-25 11:53:38 -0400

    database server.js

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({
-    host: '	l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
-    user: 'fjkdoj9rzb66qp7p',
-    password: 'llqnchds5n6lru8u',
-    database: 'xkl3dwam8m18zmv4'
-});
+// const db = mysql.createConnection({
+//     host: 
+//     user: 
+//     password: 
+//     database: 
+// });
 
 db.connect((err) => {
     if (err) {
@@ -21,20 +21,6 @@ db.connect((err) => {
     } else {
         console.log('Connected to the MySQL database');
     }
-});
-
-app.post('/store-response', (req, res) => {
-    const { totalResponsiveness, totalDemandingness, parentingStyle } = req.body;
-
-    const sql = 'INSERT INTO quiz_responses (responsiveness, demandingness, parenting_style) VALUES (?, ?, ?)';
-    db.query(sql, [totalResponsiveness, totalDemandingness, parentingStyle], (err, result) => {
-        if (err) {
-            console.error('Error storing response:', err);
-            res.status(500).json({ message: 'Error storing response.' });
-        } else {
-            res.json({ message: 'Response stored successfully.' });
-        }
-    });
 });
 
 app.listen(port, () => {
