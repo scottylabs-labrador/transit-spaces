2094aee3ade024f2a1d36a78a08cf3c5377f0c76
Xavilien <xavilien@gmail.com>
2023-11-30 23:15:18 -0500

    added getBuildings

---
+++ server/controllers/BuildingsController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { error } from "../util/error";
+import { prisma } from "../db";
+
+export const getBuildings = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    try {
+        const buildings = await prisma.building.findMany();
+        res.json(buildings);
+    } catch (err) {
+        console.error(err);
+        return error(res);
+    }
+};
