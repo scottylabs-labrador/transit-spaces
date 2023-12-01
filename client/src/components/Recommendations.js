5e3d17ad47ee26408663b3f6e8ec41f6548c84c0
Xavilien <xavilien@gmail.com>
2023-11-30 23:24:29 -0500

    added comment

--- client/src/components/Recommendations.js
+++ client/src/components/Recommendations.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Recommendations() {
+  // Leaving this here in case we want to recommend more than one option in the future
   const navigate = useNavigate();
 
   const [recommendedSeats, setRecommendedSeats] = useState([]);
